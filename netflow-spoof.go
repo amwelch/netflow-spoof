f775b424f551acc8a06994f55cf86aba290d6598
Sandy <amwelch@umich.edu>
2015-04-23 02:20:55 +0000

    [l2, l4]

--- netflow-spoof.go
+++ netflow-spoof.go
@@ -8,14 +8,24 @@ import (
 )
 
 func main() {
-	ip := &layers.IPv4{
-		SrcIP: net.IP{1, 2, 3, 4},
-		DstIP: net.IP{5, 6, 7, 8},
-	}
+
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{}
-	err := ip.SerializeTo(buf, opts)
-	if err != nil {panic(err) }
 	
-	fmt.Println(buf.Bytes())
+	l2 := &layers.Ethernet{}
+	l3 := &layers.IPv4{
+		SrcIP: net.IP{1, 2, 3, 4},
+		DstIP: net.IP{5, 6, 7, 8},
+        }
+	l4 := &layers.TCP{}
+
+
+	//LayerCake
+	gopacket.SerializeLayers(buf, opts,
+		l2, 
+		l3,
+		l4,
+		gopacket.Payload([]byte{1, 2, 3, 4}))
+	packetData := buf.Bytes()
+        fmt.Println(packetData)
 }
