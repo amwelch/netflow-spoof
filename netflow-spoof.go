908734ec52a1ffc8e287271090f732e02384f2e8
Sandy <amwelch@umich.edu>
2015-04-24 00:39:47 +0000

    start to factor out the different layers

--- netflow-spoof.go
+++ netflow-spoof.go
@@ -7,17 +7,29 @@ import (
 	"code.google.com/p/gopacket"
 )
 
+func construct_ethernet() *layers.Ethernet {
+	return &layers.Ethernet{}
+}
+
+func construct_ip(srcaddr string, dstaddr string) *layers.IPv4 {
+	return &layers.IPv4{
+		SrcIP: net.ParseIP(srcaddr),
+		DstIP: net.ParseIP(dstaddr),
+	}
+}
+
+func construct_udp() *layers.UDP {
+	return &layers.UDP{}
+}
+
 func main() {
 
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{}
 	
-	l2 := &layers.Ethernet{}
-	l3 := &layers.IPv4{
-		SrcIP: net.IP{1, 2, 3, 4},
-		DstIP: net.IP{5, 6, 7, 8},
-        }
-	l4 := &layers.TCP{}
+	l2 := construct_ethernet()
+	l3 := construct_ip("1.2.3.4", "5.6.7.8")
+	l4 := construct_udp()
 
 
 	//LayerCake
@@ -25,7 +37,7 @@ func main() {
 		l2, 
 		l3,
 		l4,
-		gopacket.Payload([]byte{1, 2, 3, 4}))
+		gopacket.Payload([]byte{9, 10, 11, 12}))
 	packetData := buf.Bytes()
         fmt.Println(packetData)
 }
