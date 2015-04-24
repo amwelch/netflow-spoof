ffebc028e2b8ef4c08aa9270d0c2cb898f5dbad5
Sandy <amwelch@umich.edu>
2015-04-24 00:56:24 +0000

    start to add v5 header + body structs

--- netflow-spoof.go
+++ netflow-spoof.go
@@ -22,6 +22,45 @@ func construct_udp() *layers.UDP {
 	return &layers.UDP{}
 }
 
+type NFLOW_v5_header struct {
+	int16 version
+	int16 count
+	int32 sys_uptime
+	int32 unix_secs
+	int32 unix_nsecs
+	int32 flow_sequence
+	int8 engine_type
+	int8 engine_id
+	int16 sampling_interval
+}
+
+type NFLOW_v5_body struct {
+	int32 srcaddr
+	int32 dstaddr
+	int32 nexthop
+	int16 input
+	int16 output
+	int32 dPkts
+	int32 dOctets
+	int32 first
+	int32 last
+	int16 srcport
+	int16 dstport
+	int8 pad1
+	int8 tcp_flags
+	int8 prot
+	int8 tos
+	int16 src_as
+	int16 dst_as
+	int8 src_mask
+	int8 dst_mask
+	int16 pad2
+}
+
+func construct_nflow_v5() {
+
+}
+
 func main() {
 
 	buf := gopacket.NewSerializeBuffer()
